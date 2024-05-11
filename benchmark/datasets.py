59951fac65aedf098aac931e8f946874d58ea1d3
dashakoryakovskaya <dasha_cat27@mail.ru>
2024-05-11 12:57:20 +0300

    Add more datasets in evaluation

--- benchmark/datasets.py
+++ benchmark/datasets.py
@@ -1,9 +1,9 @@
-from torchvision.datasets import CIFAR10, CIFAR100, DTD, Food101, OxfordIIITPet
+from torchvision.datasets import CIFAR10, CIFAR100, DTD, Food101, OxfordIIITPet, GTSRB, MNIST, SUN397, Country211, EuroSAT, FGVCAircraft, Flowers102
 import torchvision
 import json
 from typing import Tuple, List, Union
 
-all_datasets = ["cifar10", "cifar100", "dtd", "food101", "oxfordiiitpet"]
+all_datasets = ["cifar10", "cifar100", "dtd", "food101", "oxfordiiitpet", "mnist", "country211", "fgvcircraft", "flowers102"]
 
 
 def get_dataset(
@@ -15,6 +15,10 @@ def get_dataset(
         torchvision.datasets.dtd.DTD,
         torchvision.datasets.food101.Food101,
         torchvision.datasets.oxford_iiit_pet.OxfordIIITPet,
+        torchvision.datasets.mnist.MNIST,
+        torchvision.datasets.country211.Country211,
+        torchvision.datasets.fgvc_aircraft.FGVCAircraft,
+        torchvision.datasets.flowers102.Flowers102
     ],
     List[str],
 ]:
@@ -28,9 +32,24 @@ def get_dataset(
         dataset = Food101(root=".", split=split, download=True)
     elif dataset_name == "oxfordiiitpet":
         dataset = OxfordIIITPet(root=".", split=split, download=True)
+    elif dataset_name == "mnist":
+        dataset = MNIST(root=".", download=True)
+    elif dataset_name == "country211":
+        dataset = Country211(root=".", split=split, download=True)
+    elif dataset_name == "eurosat":
+        dataset = EuroSAT(root=".", download=True)
+    elif dataset_name == "fgvcircraft":
+        dataset = FGVCAircraft(root=".", split=split, download=True)
+    elif dataset_name == "flowers102":
+        dataset = Flowers102(root=".", split=split, download=True)
+
     if language == "ru":
         with open("./labels-ru/" + dataset_name + "-labels-ru.json") as f:
             labels = json.load(f)
     else:
-        labels = dataset.classes
+        if dataset_name in ["flowers102", "country211"]:
+            with open("./labels-en/" + dataset_name + "-labels-en.json") as f:
+                labels = json.load(f)
+        else:
+            labels = dataset.classes
     return dataset, labels
